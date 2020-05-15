b2a34007cdcc0be2beef2566a8fc47277b6a5ff0
Halyson <halyson97@gmail.com>
2020-05-15 12:23:42 -0300

    test executando callback quando usuario deixa a pagina

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
 import OnLeaveIntent from './index'
 
 describe('OnLeaveIntent', () => {
-  it('should return 5', () => {
-    expect(5).toBe(5)
+  it('chamar callback quando usuario sair da tela', () => {
+    const callback = jest.fn()
+
+    const onLeaveIntent = new OnLeaveIntent(callback)
+
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
+
+    expect(callback).toHaveBeenCalled()
+  })
+
+  it('nao chamar callback quando usuario nao sair da tela', () => {
+    const callback = jest.fn()
+
+    const onLeaveIntent = new OnLeaveIntent(callback)
+
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: new EventTarget() }))
+
+    expect(callback).not.toHaveBeenCalled()
   })
 })
