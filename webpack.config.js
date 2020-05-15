e288789ecc6d7b9b21dc067b9e4ad4afea8d6d5e
Halyson <halyson97@gmail.com>
2020-05-15 13:30:13 -0300

    build lib

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'on-leave-intent.min.js',
+    libraryTarget: 'umd',
+    library: 'OnLeaveIntent'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
